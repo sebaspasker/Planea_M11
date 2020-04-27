9c8dda1e4d8e6aabffaa0241a1b969500d5ce175
Sebastian Pasker <spg84@alu.ua.es>
2020-04-27 19:19:36 +0200

    Y0025435B Añadido Solicitudes y Planea aspx

--- planeaWeb/Default2.Master.designer.cs
+++ planeaWeb/Default2.Master.designer.cs
@@ -58,6 +58,15 @@ namespace planeaWeb {
         protected global::System.Web.UI.WebControls.HyperLink Modificar_Perfil;
 
         /// <summary>
+        /// Control Solicudes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink Solicudes;
+
+        /// <summary>
         /// Control Nombre_Usuario.
         /// </summary>
         /// <remarks>
